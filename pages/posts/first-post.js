08529ad6a964657658973fc78f6b6b968da09ab3
harries4206 <harries4206@gmail.com>
2023-02-08 11:13:27 +0800

    end

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
 function firstpost() {
-  return <div>first-post</div>;
+  return (
+    <>
+      <h1>first-post</h1>;
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+    </>
+  );
 }
 
 export default firstpost;
