15b29822ee2b8433c39846d4365ffaff23af9db5
harries4206 <harries4206@gmail.com>
2023-02-15 18:01:57 +0800

    delete api/data

--- pages/api/data.js
+++
@@ -1,6 +0,0 @@
-export default function data(req, res) {
-  const res = fetch(
-    `https://jsonplaceholder.typicode.com/todos?_start=${posts.length}&_limit=10`
-  );
-  res.status(200).json({ text: "hello" });
-}
